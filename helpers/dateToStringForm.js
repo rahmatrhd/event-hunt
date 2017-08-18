4d54e2e7e1ad4d5f20e1a6b672f75a514cab4d30
Rahmat Hidayat <email": null>
2017-08-18 10:05:39 +0700

    time

--- helpers/dateToStringForm.js
+++ helpers/dateToStringForm.js
@@ -1,5 +1,5 @@
 module.exports = string => {
 	let date = new Date(string)
-	date.setTime(date.getTime() + (7*60*60*1000))
+	// date.setTime(date.getTime() + (7*60*60*1000))
   return date.toISOString().split('.')[0]
 }
